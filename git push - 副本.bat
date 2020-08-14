8a69aaeb2f8da3321d222e8d61bc9a178db81b34
ting-talk <804976336@qq.com>
2020-08-15 02:03:06 +0800

    更新

--- git push - 副本.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set /p commit=commit:
-title auto commit
-D:\Program Files\Git\bin\git.exe add -A
-D:\Program Files\Git\bin\git.exe commit -m %commit%
-D:\Program Files\Git\bin\git.exe push origin master
-exit
-
